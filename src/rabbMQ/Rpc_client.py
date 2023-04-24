8869350215ed2a4c035e5a55f4330b932c6f9e9c
Sebastian <sehernandezce@unal.edu.co>
2023-04-23 22:31:17 -0500

    Cola de mensajes Docker - client

--- src/rabbMQ/Rpc_client.py
+++ src/rabbMQ/Rpc_client.py
@@ -7,14 +7,16 @@ class client(object):
     def __init__(self, urlQueue, idQueue):
         self.urlQueue = urlQueue
         self.idQueue = idQueue
+        self.connect()
+    
+    def connect(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.urlQueue))
+          pika.ConnectionParameters(host=self.urlQueue, heartbeat=360, blocked_connection_timeout=65))
 
         self.channel = self.connection.channel()
 
         result = self.channel.queue_declare(queue='', exclusive=True)
         self.callback_queue = result.method.queue
-
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
@@ -23,24 +25,30 @@ class client(object):
         self.response = None
         self.corr_id = None
 
+
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
     def call(self, item):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key=self.idQueue,
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=item)
-        self.connection.process_data_events(time_limit=None)
-        self.response = self.response.decode('utf-8')
-        return self.response
+        try: 
+            self.response = None
+            self.corr_id = str(uuid.uuid4())
+            self.channel.basic_publish(
+                exchange='',
+                routing_key=self.idQueue,
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    correlation_id=self.corr_id,
+                ),
+                body=item)
+            self.connection.process_data_events(time_limit=None)
+            self.response = self.response.decode('utf-8')
+            return self.response
+        except Exception as e:
+            print(self.response)
+            print(e)
+            return (str({ "description": "Error call rpc_client", "status_code": 500, "error": str(e)}))
 
     async def call_async(self, item):
         loop = asyncio.get_running_loop()
--- src/tutorias/mq/Client_tutorias.py
+++ src/tutorias/mq/Client_tutorias.py
@@ -1,16 +1,25 @@
-import json
 from rabbMQ.Rpc_client import client as tutoriasRpcClient
+from dotenv import load_dotenv
+import pika
+import json
+import os
 
-url = '127.0.0.1'
-urlQueue = str(url)
-idQueue = str('tutorias_rpc_queue')
+load_dotenv()
+
+urlQueue = str(os.environ.get("URI_QUEUE"))
+idQueue = str(os.environ.get("ID_QUEUE"))
 
 tutorias_rpc = tutoriasRpcClient(urlQueue, idQueue)
 
 async def send(item):
-    print(" [x] Requesting...")
-    response = await tutorias_rpc.call_async(json.dumps(item))
-    print(" [.] Got!")
-    response = json.loads(response) # convertir a diccionario
-    return response
+    try:
+        print(" [x] Requesting...")
+        response = await tutorias_rpc.call_async(json.dumps(item))
+        print(" [.] Got!")
+        response = json.loads(response) # convertir a diccionario
+        return response
+    except Exception as e:
+        print(e)
+        return (str({ "description": "Error send rpc_client", "status_code": 500, "error": str(e)}))
+ 
 
