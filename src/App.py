d1900968ea442e34595c9fc7e8e2b02154063b64
jescarraga <jescarraga@unal.edu.co>
2023-05-10 21:57:16 -0500

    permitir corse

--- src/App.py
+++ src/App.py
@@ -8,15 +8,25 @@ from autenticacion.Index import autenticacion
 from formularios.Index import formularios
 from remisiones.Index import remisiones
 from tutorias.Index import tutorias
+from starlette.middleware.cors import CORSMiddleware
 
 load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(gestionUsuarios, prefix="/gestionUsuarios")
 app.include_router(formularios, prefix="/formularios")
 app.include_router(remisiones, prefix="/remisiones")
 app.include_router(autenticacion, prefix="/auth")
 app.include_router(tutorias, prefix="/tutorias")
+
+
 
 #Development -> reload = True
 if __name__ == "__main__":
