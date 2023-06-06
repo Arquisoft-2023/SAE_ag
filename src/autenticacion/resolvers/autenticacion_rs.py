08d06f8b85e7b8e304fe5f3c9a09268bcfd800c3
Luis Mendez <81998928+LuisMendez-ING@users.noreply.github.com>
2023-06-06 00:22:45 -0500

    Modificaciones v1 auth

--- src/autenticacion/resolvers/autenticacion_rs.py
+++ src/autenticacion/resolvers/autenticacion_rs.py
@@ -24,12 +24,12 @@ class Mutation:
     #     response = requests.request("POST", f'{urlApi}/signin', json=mapper_general.to_json(self, userInGestionUsuarios))
     #     return gestion.gestionar_respuesta_micro(self, response, UsuarioAuthWithToken, "uno")
     @strawberry.mutation
-    async def signin(self, usuario_un: str, password: str) -> str:
+    async def signin(self, usuario_un: str, password: str) -> dict:
         url = f'{urlApi}/signin'
         data = {'usuario_un': usuario_un, 'password': password}
 
         response = requests.post(url, data=data)
-        content = response.text
+        content = response.json()
 
         return content
     #async def signin(self, item: UsuarioAuthInput):
